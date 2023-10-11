965d0b50b3c59350135ee9ecb3f840f6ec92f945
comdesk <deskcom317@gmail.com>
2023-10-11 15:49:36 +0900

    홈 로그 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Hero from '../components/home/hero.js'
 
 export default function Home() {
+  console.log('home invoked...')
+
   return (
     <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
